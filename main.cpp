7992609ffb3134dd58a55c76d4b3e1369c4ed1bc
Kyle Moorhead <moorheadk2335@gmail.com>
2024-01-23 20:39:45 -0600

    Add performance timing print statements

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "include/DataParser.h"
 #include "include/DirectoryParser.h"
 
+#include <chrono>
 #include <filesystem>
 #include <iostream>
 #include <memory>
@@ -14,15 +15,34 @@ void printUsage()
     cout << "Usage: ./takehome <path to data directory>" << endl;
 }
 
+void printTiming(chrono::system_clock::time_point start, chrono::system_clock::time_point stop)
+{
+    auto runTimeUSeconds = chrono::duration_cast<chrono::microseconds>(stop - start);
+    auto runTimeSeconds = chrono::duration_cast<chrono::seconds>(stop - start);
+    auto runTimeMilliseconds = chrono::duration_cast<chrono::milliseconds>(stop - start);
+
+    auto secondCount = runTimeSeconds.count();
+    auto millisecondCount = runTimeMilliseconds.count() - (runTimeSeconds.count() * 1000);
+
+    cout << "Total execution time: " << runTimeUSeconds.count() << " microseconds (" <<
+        secondCount << "." << setfill('0') << setw(3) << millisecondCount  << " seconds)" << endl;
+}
+
 void runParser(DataParser &parser)
 {
+    auto start = chrono::high_resolution_clock::now();
     if (!parser.parse()) {
         cout << "Parser failed to parse data" << endl;
         printUsage();
         return;
     }
+    auto stop = chrono::high_resolution_clock::now();
 
+    // We are removing this print from the timing since cout can add arbitrary time
     parser.printResult();
+    
+    // Optional code to print timing information
+    // printTiming(start, stop);
 }
 
 int main(int argc, char** argv)
@@ -35,6 +55,7 @@ int main(int argc, char** argv)
 
     DirectoryParser directoryParser(argv[1]);
     runParser(directoryParser);
+
 
     return 0;
 }
